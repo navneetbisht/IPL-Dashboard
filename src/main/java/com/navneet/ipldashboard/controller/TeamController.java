73f7e46eb03bee523a834bf9065a3322e2ac59cc
Navneet Bisht <nbisht21@gmail.com>
2021-09-09 08:54:10 +0530

    Added Team API endpoint

--- src/main/java/com/navneet/ipldashboard/controller/TeamController.java
+++ src/main/java/com/navneet/ipldashboard/controller/TeamController.java
@@ -3,15 +3,10 @@ package com.navneet.ipldashboard.controller;
 import com.navneet.ipldashboard.model.Team;
 import com.navneet.ipldashboard.repository.MatchRepository;
 import com.navneet.ipldashboard.repository.TeamRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class TeamController {
@@ -28,8 +23,7 @@ public class TeamController {
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = this.teamRepository.findByTeamName(teamName);
-        Pageable pageable = PageRequest.of(0, 4);
-        team.setMatches(matchRepository.getByTeam1OrTeam2orderByDateDesc(teamName, teamName, pageable));
+        team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
 
         return team;
 
--- src/main/java/com/navneet/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/navneet/ipldashboard/repository/MatchRepository.java
@@ -1,13 +1,18 @@
 package com.navneet.ipldashboard.repository;
 
 import com.navneet.ipldashboard.model.Match;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
-    List<Match> getByTeam1OrTeam2orderByDateDesc(String teamName1,String teamName2);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1,String teamName2, Pageable pageable);
 
+    default List<Match> findLatestMatchesbyTeam(String teamName, int count) {
 
+        return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
+    }
 }
